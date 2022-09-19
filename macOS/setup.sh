2c47d785ee13ff1f27b844c3e236e452e240570a
Yanick Senn <ys-services@yanicksenn.com>
2022-09-19 15:32:21 +0200

    Small refactoring of setup script

--- macOS/setup.sh
+++ macOS/setup.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+log() {
+  echo "$1" | tee -a "$LOGFILE"
+}
+
 confirm() {
   local message=$1
 
@@ -24,65 +28,165 @@ command_exists() {
   fi
 }
 
-install_xcode() {
+setup_xcode() {
   if ! xcode-select -p &> /dev/null; then
-    xcode-select --install
+    log "  Installing xcode ..."
+    xcode-select --install >> "$LOGFILE"
+    log "  xcode installed"
   else
-    echo "xcode already installed"
+    log "  xcode already installed"
   fi
 }
 
-install_brew() {
+setup_brew() {
   if ! command_exists "brew"; then
-     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    log "  Installing brew ..."
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" >> "$LOGFILE"
+    log "  brew installed"
   else
-    echo "brew already installed"
+    log "  brew already installed"
   fi
 }
 
-install_sdkman() {
-  if ! command_exists "sdk"; then
-    curl -s "https://get.sdkman.io" | bash
-    source "$HOME/.sdkman/bin/sdkman-init.sh"
+brew_install() {
+  local name=$1
+
+  if ! brew list "$name" &> /dev/null; then
+    log "  Installing $name ..."
+    brew install "$name" >> "$LOGFILE"
+    log "  $name installed"
   else
-    echo "sdkman already installed"
+    log "  $name already installed"
   fi
+}
+
+brew_install_cask() {
+  local name=$1
+
+  if ! brew list "$name" &> /dev/null; then
+    log "  Installing $name ..."
+    brew install --cask "$name" >> "$LOGFILE"
+    log "  $name installed"
+  else
+    log "  $name already installed"
+  fi
+}
+
+setup_sdkman() {
+  if ! command_exists "sdk"; then
+    log "  Installing sdkman ..."
+    curl -s "https://get.sdkman.io" | bash >> "$LOGFILE"
+    source "$HOME/.sdkman/bin/sdkman-init.sh"
+    log "  sdkman installed"
+  else
+    log "  sdkman already installed"
+  fi
+}
+
+sdkman_install() {
+  local name=$1
+  local version=$2
+
+  local candidate=~/.sdkman/candidates/$name/$version
+
+  if [ ! -d "$candidate" ]; then
+    log "  Installing $name $version ..."
+    sdkman_install "$name" "$version" >> "$LOGFILE"
+    log "  $name $version installed"
+  else
+    log "  $name $version already installed"
+  fi
+}
+
+
+# Python installation
+
+install_pyenv() {
+  brew_install pyenv
+}
+
+install_pyenv_update() {
+  local path=$(pyenv root)/plugins/pyenv-update
+
+  if [ ! -d "$path" ]; then
+    log "  Installing pyenv-update ..."
+    git clone https://github.com/pyenv/pyenv-update.git "$path" >> "$LOGFILE"
+    log "  pyenv-update installed"
+  else
+    log "  pyenv-update already installed"
+  fi
+}
+
+install_python_version() {
+  local version=$1
+
+  if ! pyenv versions --bare | grep -q "$version"; then
+    log "  Installing python $version ..."
+    pyenv install -s "$version" >> "$LOGFILE"
+    log "  python $version installed"
+  else
+    log "  python $version already installed"
+  fi
+}
+
+
+# Main setup
+
+setup() {
+  log "Setup workspace - macOS"
+  log "  Version: $VERSION"
+  log "  Logfile: $LOGFILE"
+  log ""
+
+  if ! confirm "Do you want to continue?"; then
+    log "User abort"
+    exit 1
+  fi
+  log ""
+
+  # Install prerequisites
+  log "Installing prerequisites ..."
+  setup_xcode
+  setup_brew
+  log ""
+
+  # Install Python & Tools
+  log "Installing Python & Tools ..."
+  install_pyenv
+  install_pyenv_update
+  install_python_version "$PYTHON_VERSION"
+  log ""
+
+  # Install JDK & JVM Tools
+  log "Installing JDK & JVM Tools ..."
+  setup_sdkman
+  sdkman_install java 17.0.2-open
+  sdkman_install kotlin
+  sdkman_install maven
+  sdkman_install gradle
+  log ""
+
+  # Install dev-tools
+  log "Installing dev-tools ..."
+  brew_install_cask jetbrains-toolbox
+  brew_install_cask visual-studio-code
+  brew_install_cask cyberduck
+  log ""
+
+  # Install utilities
+  log "Installing utilities ..."
+  brew_install_cask google-chrome
+  brew_install_cask grammarly-desktop
+  brew_install_cask 1password
+  brew_install_cask 1password-cli
+
+  log ""
+  log "Setup done."
 }
 
 VERSION=1.0.0
+LOGFILE=$(mktemp ~/setuplog.XXXXXXXX) || exit 1
 
-echo "Setup workspace - macOS"
-echo "  Version: $VERSION"
-echo ""
+PYTHON_VERSION=3.10.6
 
-if ! confirm "Do you want to continue?"; then
-  echo "User abort"
-  exit 1
-fi
-
-# Install prerequisites
-install_xcode
-install_brew
-install_sdkman
-
-# Install Python
-brew install python@3.10
-
-# Install JDK and JVM Tools
-sdk install java 17.0.2-open
-sdk install kotlin
-sdk install maven
-sdk install gradle
-
-# Install dev-tools
-brew install --cask jetbrains-toolbox
-brew install --cask visual-studio-code
-brew install --cask cyberduck
-
-# Install utilities
-brew install --cask google-chrome
-brew install --cask grammarly-desktop
-brew install --cask 1password
-
-echo ""
-echo "Setup done."
+setup
